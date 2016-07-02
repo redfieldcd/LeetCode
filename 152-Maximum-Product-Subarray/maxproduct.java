df063877aee16f0b7cd80d35c1574f1d8cdd97c9
Dan Cao <withdancao@gmail.com>
2016-07-01 18:34:53 -0700

    MAX product

--- 152-Maximum-Product-Subarray/maxproduct.java
+++ 152-Maximum-Product-Subarray/maxproduct.java
@@ -1,19 +1,17 @@
 public class Solution {
-    public int maxProduct(int[] nums) {
-        int n = nums.length;
-        
-        int max = nums[0];
-        int min = nums[0];
-        int reuslt = nums[0];
-        for(int i = 1; i < n; i++) {
+    public int maxProduct(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int max = A[0], min = A[0], result = A[0];
+        for (int i = 1; i < A.length; i++) {
             int temp = max;
-            max = Math.max(Math.max(max*nums[i], min*nums[i]), nums[i]);
-            min = Math.min(Math.min(temp*nums[i], min*nums[i]), nums[i]);
+            max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
+            min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
             if (max > result) {
                 result = max;
             }
         }
-        
-        return max;
+        return result;
     }
 }
