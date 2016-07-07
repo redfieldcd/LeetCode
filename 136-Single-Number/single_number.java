88bc7bb9c4b458f53f0fae1f0573115bf5cebc29
Dan Cao <withdancao@gmail.com>
2016-07-06 19:22:44 -0700

    SingleNumber by HashSet

--- 136-Single-Number/single_number.java
+++ 136-Single-Number/single_number.java
@@ -1,19 +1,21 @@
 public class Solution {
+    // xor
+    // public int singleNumber(int[] nums) {
+    //     int result = 0;
+    //     for(int i = 0; i < nums.length; i++) {
+    //         result ^= nums[i];
+    //     }
+    //     return result;
+    // }
+    
+    // hashset
     public int singleNumber(int[] nums) {
-        int result = 0;
+        Set<Integer> set = new HashSet<Integer>();
         for(int i = 0; i < nums.length; i++) {
-            result ^= nums[i];
+          if(!set.add(nums[i])) {
+              set.remove(nums[i]);
+          } 
         }
-        
-        return result;
-        // Set<Integer> set = new HashSet<Integer>();
-        // for(int i = 0; i > nums.length; i++) {
-        //   if(set.contains(nums[i])) {
-        //       return nums[i];
-        //   } else {
-        //       set.add(nums[i]);
-        //   }
-        // }
-        // return -1;
+        return set.iterator().next();
     }
 }
