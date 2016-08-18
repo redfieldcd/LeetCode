3947cb161f3a1bc63a2d1765b10038b0abc6a08c
Dan Cao <withdancao@gmail.com>
2016-08-18 10:46:45 +0800

    Binary Tree Paths

---
+++ 257-Binary-Tree-Paths/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ //简单的二叉树遍历，分左右子树
+public class Solution {
+    List<String> list = new ArrayList<String>();
+    
+    public List<String> binaryTreePaths(TreeNode root) {
+        String s = "" + root.val;
+        if (root != null) findPaths(root, s);
+        
+        return list;
+    }
+    
+    
+    public void findPaths(TreeNode root, String s) {
+        if(root.left == null && root.right == null) {
+            list.add(s);
+        } 
+        //左右子树是要分别考虑的，所以不能写elseif
+        if (root.left != null) {
+            findPaths(root.left, s+"->"+root.left.val);
+        } 
+        
+        if (root.right != null) {
+            findPaths(root.right, s+"->"+root.right.val);
+        }
+    }
+}
