b26a17bfed8f732dacc5db06128928295a3b46e1
Dan Cao <withdancao@gmail.com>
2016-08-18 13:33:50 +0800

    Populating Next Right Pointers in Each Node

--- 116-Populating-Next-Right-Pointers-in-Each-Node/solution.java
+++ 116-Populating-Next-Right-Pointers-in-Each-Node/solution.java
@@ -11,19 +11,18 @@
 public class Solution {
     public void connect(TreeLinkNode root) {
         Queue<TreeLinkNode> q = new LinkedList<TreeLinkNode>();
-        if(root!=null) q.offer(root);
-        while(!q.isEmpty()){
-            //记录本层节点的个数
+        if(root != null) q.offer(root);
+        while(!q.isEmpty()) {
             int size = q.size();
-            for(int i = 0; i < size; i++){
-                TreeLinkNode curr = q.poll();
-                //最后一个节点的next是null，不做处理
-                if(i < size - 1){
+            for(int i = 0; i < size; i++) {
+                TreeLinkNode node = q.poll();
+                // 最后一个节点的next已经被初始为null，不用管它
+                if(i < size - 1) {
                     TreeLinkNode next = q.peek();
-                    curr.next = next;
-                }
-                if(curr.left != null) q.offer(curr.left);
-                if(curr.right != null) q.offer(curr.right);
+                    node.next = next;
+                }   
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
             }
         }
     }
