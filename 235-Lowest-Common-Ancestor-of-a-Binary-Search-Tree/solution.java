f55447d6573fa7ab2d91a28ecf07f1b4bda4b56d
Dan Cao <withdancao@gmail.com>
2016-08-18 13:05:32 +0800

    Lowest Common Ancestor of a Binary Search Tree

---
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+//给出了root,顺着root找,如果root的大小比两个都大，那么，LCA在
+//左子树，如果root的大小比两个都笑,那么LCA在右子树
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+}
