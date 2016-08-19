4655b4b4a172137359c50fe17bab80e491077f01
Dan Cao <withdancao@gmail.com>
2016-08-19 14:31:04 +0800

    Triangle

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -2,7 +2,7 @@
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         for(int i = triangle.size()-2; i>=0; i--) {
-            for(int j = 0; j < triangle.get(i).size(); i++) {
+            for(int j = 0; j < triangle.get(i).size(); j++) {
                 int min = Math.min(triangle.get(i+1).get(j), triangle.get(i+1).get(j+1));
                 triangle.get(i).set(j, triangle.get(i).get(j)+ min);
             }
