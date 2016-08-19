62ed677dd52887de211ab25896243b3eb2278388
Dan Cao <withdancao@gmail.com>
2016-08-19 14:43:30 +0800

    Unique Paths

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n]
+        int[][] dp = new int[m][n];
         for(int i = 0; i < m; i++) {
             dp[i][0] = 1;
         }
