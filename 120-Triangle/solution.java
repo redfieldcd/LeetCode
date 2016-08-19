8f9f2bfc659ef078dc2bef05965984b9a002a19e
Dan Cao <withdancao@gmail.com>
2016-08-19 14:25:01 +0800

    Triangle

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -1,7 +1,7 @@
 // 从下往上的递归，最后结果就在顶点，适合画图理清楚逻辑的题目
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        for(int i = triangle.size()-2; i>=0; i++) {
+        for(int i = triangle.size()-2; i>=0; i--) {
             for(int j = 0; j < triangle.get(i).size(); i++) {
                 int min = Math.min(triangle.get(i+1).get(j), triangle.get(i+1).get(j+1));
                 triangle.get(i).set(j, triangle.get(i).get(j)+ min);
