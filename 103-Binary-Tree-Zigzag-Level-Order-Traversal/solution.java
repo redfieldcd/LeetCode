1765c47ac33de8a40d63d2f0c184c204292e5fdb
Dan Cao <withdancao@gmail.com>
2016-08-19 09:19:59 +0800

    [103. Binary Tree Zigzag Level Order Traversal][Runtime Error]committed by LeetCode Extension

---
+++ 103-Binary-Tree-Zigzag-Level-Order-Traversal/solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> list = new LinkedList();
+        Queue<TreeNode> q = new LinkedList();
+        
+        q.offer(root);
+        
+        int mark = 0;
+        while(!q.isEmpty()) {
+            LinkedList<Integer> temp = new LinkedList();
+            int size = q.size();
+
+            for(int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if(mark%2 == 0) {
+                   temp.add(node.val); 
+                } else {
+                   temp.add(0, node.val);
+                }
+                
+                if(node.left != null) {
+                    q.offer(node.left);
+                }
+                
+                if(node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+            list.add(temp);
+            mark++;
+        }
+        return list;
+    }
+}
