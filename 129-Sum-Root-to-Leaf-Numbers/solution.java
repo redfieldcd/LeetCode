9bea86068fd0752a63aa1d09b5f8a35510d88f64
Dan Cao <withdancao@gmail.com>
2016-08-19 09:44:02 +0800

    Sum Root to Leaf Numbers

---
+++ 129-Sum-Root-to-Leaf-Numbers/solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ //二叉树遍历，在遍历的过程中将自身的数值加入到叶子节点中
+public class Solution {
+    int sum = 0;
+    public int sumNumbers(TreeNode root) {
+        if(root != null) getSum(root);
+        return sum;
+    }
+    
+    publicc void getSum(TreeNode root) {
+        if(root.left == null && root.right == null) {
+            sum += root.val;
+        }
+        
+        if(root.left != null) {
+            root.left.val += root.val*10
+            getSum(root.left);
+        }
+        
+        if(root.right != null) {
+            root.right.val += root.val*10;
+            getSum(root.right);
+        }
+    }
+}
