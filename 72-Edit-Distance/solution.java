889b92ece1218fa933d3c5f2b53d995081d903ca
Dan Cao <withdancao@gmail.com>
2016-08-19 12:08:43 +0800

    Edit Distance

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -1,27 +1,53 @@
+// public class Solution {
+//     public int minDistance(String word1, String word2) {
+//         int m = word1.length();
+//         int n = word2.length();
+        
+//         int[][] res = new int[m+1][n+1];
+        
+//          for(int i = 1; i <= m; i++) {
+//             res[i][0] = i;
+//         }
+        
+//         for(int i = 1; i <= n; i++) {
+//             res[0][i] = i;
+//         }
+     
+//         for(int i = 1; i <= m; i++) {
+//             for(int j = 1; i <= n; j++) {
+//                 int insertion = res[i][j-1] + 1;
+//                 int deletion = res[i-1][j] + 1;
+//                 int replace = res[i-1][j-1] + (word1.charAt(i-1) == word2.charAt(j-1) ? 0 : 1);
+//                 res[i][j] = Math.min(replace, Math.min(insertion, deletion));
+//             }
+//         }
+//         return res[m][n];
+//     }
+// }
+
 public class Solution {
     public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-        
-        int[][] res = new int[m+1][n+1];
-        
-        for(int i = 1; i <= n; i++) {
-            res[0][i] = i;
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        // 初始化空字符串的情况
+        for(int i = 1; i <= m; i++){
+            dp[i][0] = i;
         }
-        
-        for(int i = 1; i <= m; i++) {
-            res[i][0] = i;
+        for(int i = 1; i <= n; i++){
+            dp[0][i] = i;
         }
-        
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; i <= n; j++) {
-                int insertion = res[i][j-1] + 1;
-                int deletion = res[i-1][j] + 1;
-                int replace = res[i-1][j-1] + (word1.charAt(i-1) == word2.charAt(j-1) ? 0 : 1);
-                res[i][j] = Math.min(Math.min(insertion, deletion), replace);
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                // 增加操作：str1a变成str2后再加上b，得到str2b
+                int insertion = dp[i][j-1] + 1;
+                // 删除操作：str1a删除a后，再由str1变为str2b
+                int deletion = dp[i-1][j] + 1;
+                // 替换操作：先由str1变为str2，然后str1a的a替换为b，得到str2b
+                int replace = dp[i-1][j-1] + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1);
+                // 三者取最小
+                dp[i][j] = Math.min(replace, Math.min(insertion, deletion));
             }
         }
-        
-        return res[m][n];
+        return dp[m][n];
     }
 }
