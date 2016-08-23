f3b5aab0ad05bfac35e3cc3a545f26d582f9923e
Dan Cao <withdancao@gmail.com>
2016-08-23 15:53:53 +0800

    Subsets

---
+++ 78-Subsets/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList();
+        List<Integer> temp = new LinkedList();
+        
+        res.add(temp);
+        helper(nums, 0, temp, res);
+        
+        return res;
+    }
+    
+    public void helper(int[] nums, int idx, List<Integer> temp, List<List<Integer>> res) {
+        if(idx >= nums.length) return;
+        //不包含这个元素的
+        helper(nums, idx+1, temp, res);
+        //新的集合要new一个新的list，防止修改引用。
+        List<Integer> temp2 = new LinkedList(temp);
+        temp2.add(nums[idx]);
+        res.add(temp2);
+        //包含这个元素的
+        helper(nums, idx+1, temp2, res);
+    }
+}
