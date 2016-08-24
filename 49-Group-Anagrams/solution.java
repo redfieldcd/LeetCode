7199a96ae05f8ff15475d51a0a2b1bcea45b9414
Dan Cao <withdancao@gmail.com>
2016-08-24 09:39:15 +0800

    Group Anagrams

---
+++ 49-Group-Anagrams/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+         List<List<String>> res = new LinkedList();
+         Map<String, List<String>> map = new HashMap<String, List<String>>();
+         
+         for(int i = 0; i < strs.length; i++) {
+             char[] s = strs[i].toCharArray();
+             Arrays.sort(s);
+             //一定要new 一个string
+             String key = new String(s);
+             List<String> list = map.get(key);
+             if(list == null) {
+                list = new LinkedList();
+             }
+             
+             list.add(strs[i]);
+             map.put(key, list);
+         }
+         
+         for(String i : map.keySet()) {
+             List<String> cur = map.get(i);
+             Collections.sort(cur);
+             res.add(cur);
+         }
+         
+         return res;
+    }
+}
