1cd0f7c6b6297ce581162a0b27577200edbf0b30
Dan Cao <withdancao@gmail.com>
2016-08-24 10:29:55 +0800

    Populating Next Right Pointers in Each Node II

--- 117-Populating-Next-Right-Pointers-in-Each-Node-II/solution.java
+++ 117-Populating-Next-Right-Pointers-in-Each-Node-II/solution.java
@@ -6,41 +6,38 @@
  *     TreeLinkNode(int x) { val = x; }
  * }
  */
-public class Solution {
+ public class Solution {
     //based on level order traversal
     public void connect(TreeLinkNode root) {
-        TreeLinkNode head = null; //head of the next level
-        TreeLinkNode prev = null; //the leading node on the next level
-        TreeLinkNode cur = root;  //current node of current level
-
-        while (cur != null) {
-            while (cur != null) { //iterate on the current level
-                //left child
-                if (cur.left != null) {
-                    if (prev != null) {
+        TreeLinkNode head = null;
+        TreeLinkNode pre = null;
+        TreeLinkNode cur = root;
+        
+        while(cur != null) {
+            while(cur != null) {
+                if(cur.left != null) {
+                    if(pre != null) {
                         prev.next = cur.left;
                     } else {
                         head = cur.left;
                     }
-                    prev = cur.left;
                 }
-                //right child
-                if (cur.right != null) {
-                    if (prev != null) {
-                        prev.next = cur.right;
+                
+                pre = cur.left;
+                
+                if(cur.right != null) {
+                    if(pre != null) {
+                        pre.next = cur.right;
                     } else {
                         head = cur.right;
                     }
-                    prev = cur.right;
                 }
-                //move to next node
-                cur = cur.next;
+                pre =cur.right;
             }
             
-            //move to next level
             cur = head;
+            pre = null;
             head = null;
-            prev = null;
         }
         
     }
