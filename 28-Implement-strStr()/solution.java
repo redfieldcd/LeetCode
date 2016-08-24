43b8fe1bc32f12501cd22026a3b48c937c9dcd4d
Dan Cao <withdancao@gmail.com>
2016-08-24 09:09:10 +0800

    [28. Implement strStr()][Compile Error]committed by LeetCode Extension

---
+++ 28-Implement-strStr()/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        int len = needle.length();
+        int l = haystack.length();
+        if(l < len) return -1;
+        
+        for(int i = 0; i <= l - len; i++) {
+            if(haystack.substring(i, i+len).equals(needle)) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
