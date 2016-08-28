ce9d3f971ab161a1328a650dc36e590135dc6f5c
Dan Cao <withdancao@gmail.com>
2016-08-29 03:07:25 +0800

    Clone Graph

--- 133-Clone-Graph/solution.java
+++ 133-Clone-Graph/solution.java
@@ -6,14 +6,38 @@
  *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
  * };
  */
+ 
+//  public class Solution {
+//     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+//         if (node == null) return null;
+//         UndirectedGraphNode root = new UndirectedGraphNode(node.label);//复制根节点
+//         Queue<UndirectedGraphNode> queue = new LinkedList<>();
+//         Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+//         queue.offer(node);//queue放入根结点
+//         map.put(node, root);//map放入根结点和它的复制结点
+//         while (!queue.isEmpty()) {
+//             UndirectedGraphNode cur = queue.poll();//取出queue中（同一层）的结点进行BFS
+//             for (UndirectedGraphNode n: cur.neighbors) {
+//                 //对没有复制过的结点进行复制，并将这个结点放入queue
+//                 if (!map.containsKey(n)) {
+//                     map.put(n, new UndirectedGraphNode(n.label));
+//                     queue.offer(n);
+//                 }
+//                 //连接复制结点和复制neighbor结点
+//                 map.get(cur).neighbors.add(map.get(n));
+//             }
+//         }
+//         return root;
+//     }
+// }
 public class Solution {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if(node == null) return null;
-        UndirectedGraphNode root = new UndirectedGraphNode(node);
+        UndirectedGraphNode root = new UndirectedGraphNode(node.label);
         Queue<UndirectedGraphNode> q = new LinkedList();
         Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap();
         
-        q.offer(root);
+        q.offer(node);
         map.put(node, root);
         
         while(!q.isEmpty()) {
