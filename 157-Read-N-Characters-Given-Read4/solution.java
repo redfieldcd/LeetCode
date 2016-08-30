1b29c2c64b423c1a02c83e4f508c2aca84646cfa
Dan Cao <withdancao@gmail.com>
2016-08-29 21:36:49 -0400

    Read N Characters Given Read4

--- 157-Read-N-Characters-Given-Read4/solution.java
+++ 157-Read-N-Characters-Given-Read4/solution.java
@@ -2,17 +2,21 @@
       int read4(char[] buf); */
 
 public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
     public int read(char[] buf, int n) {
-        for(int i = 0; i < n; i += 4){
-            char[] tmp = new char[4];
-            // 将数据读入临时数组
-            int len = read4(tmp);
-            // 将临时数组拷贝至buf数组，这里拷贝的长度是本次读到的个数和剩余所需个数中较小的
-            System.arraycopy(tmp, 0, buf, i, Math.min(len, n - i));
-            // 如果读不满4个，说明已经读完了，返回总所需长度和目前已经读到的长度的较小的
-            if(len < 4) return Math.min(i + len, n);
+        for(int i = 0; i < buf.length; i+4) {
+            char[] temp = new char[4];
+            int len = read4(temp);
+            
+            System.arraycopy(temp, 0, buf, i, Math.min(len, n - i));
+            
+            if(len < 4) return Math.min(i+len, n);
         }
-        // 如果循环内没有返回，说明读取的字符是4的倍数
+        
         return n;
     }
 }
