6f91189474c11affbd55276a6321b570a4bc4d42
Dan Cao <withdancao@gmail.com>
2016-08-29 21:58:28 -0400

    [58. Length of Last Word][Accepted]committed by LeetCode Extension

--- 58-Length-of-Last-Word/solution.java
+++ 58-Length-of-Last-Word/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public int lengthOfLastWord(String s) {
         String[] str = s.split(" ");
+        if(str.length < 1) return 0;
         String word = str[str.length-1];
         
         return word.length();
