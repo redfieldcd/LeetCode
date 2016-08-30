cbfb07e11c91027a12426b3b3b005e1d834a36b3
Dan Cao <withdancao@gmail.com>
2016-08-29 21:23:52 -0400

    Read N Characters Given Read4

---
+++ 157-Read-N-Characters-Given-Read4/solution.java
@@ -0,0 +1,22 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        for(int i = 0; i < buf.length; i++) {
+            int[] temp = new int[4];
+            int len = read4(temp);
+            
+            System.arraycopy(temp, 0, buf, i, Math.min(len, n - i));
+            
+            if(len < 4) return Math.min(i+len, n);
+        }
+        
+        return n;
+    }
+}
