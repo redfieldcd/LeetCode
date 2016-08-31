c8c020c608a1f5d1dbc406fd2a9dc2a618fbdff2
Dan Cao <withdancao@gmail.com>
2016-08-31 09:54:01 -0400

    H-Index II

---
+++ 275-H-Index-II/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int hIndex(int[] citations) {
+         int n = citations.length;
+         if(n == 0) return 0;
+         int min = 0;
+         int max = n - 1;
+         
+         while(min <= max) {
+             int mid = min + (max - min) / 2;
+             if(citations[mid] < n - mid) {
+                 min = mid + 1;
+             } else {
+                 max = mid - 1;
+             }
+         }
+         
+         return n - min;
+    }
+}
