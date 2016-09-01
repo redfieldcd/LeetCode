d804b2fb021184616b346d343b7f96e26dc66f72
Dan Cao <withdancao@gmail.com>
2016-09-01 13:07:25 -0400

    Kth Smallest Element in a Sorted Matrix

--- 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
@@ -1,5 +1,6 @@
 public class Solution {
     public int kthSmallest(int[][] matrix, int k) {
+          if(matrix.length == 1) return matrix[0][k-1];
           int row = k / matrix.length;
           int col = (k-1) % matrix[0].length;
           return matrix[row][col];
