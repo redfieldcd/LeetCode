5bd7fdee5bca37f97151fe290f4268939767949b
Dan Cao <withdancao@gmail.com>
2016-09-01 12:53:51 -0400

    Two Sum II - Input array is sorted

---
+++ 167-Two-Sum-II---Input-array-is-sorted/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int low = 0, high = numbers.length - 1;
+        
+        while(low < high) {
+            int sum = numbers[low] + numbers[high];
+            if(sum == target) return new int[]{low + 1, high + 1};
+            if(sum > target) high--;
+            else low++;
+        }
+        
+        return new int[]{-1, -1};
+    }
+}
