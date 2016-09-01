d625d8976d0faf8c9fd95d67d11a316e0ba0b194
Dan Cao <withdancao@gmail.com>
2016-09-01 10:12:52 -0400

    Guess Number Higher or Lower

--- 374-Guess-Number-Higher-or-Lower/solution.java
+++ 374-Guess-Number-Higher-or-Lower/solution.java
@@ -3,15 +3,16 @@
    @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
       int guess(int num); */
 
+//my number means 猜的人说的那个数
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
         int low = 0, high = n;
         
         while(low + 1 < high) {
             int mid = low + (high - low) / 2;
-            if(guess(mid) == 0) return guess;
-            if(guess(mid) < 0) low = mid;
-            else high = mid;
+            if(guess(mid) == 0) return mid;
+            if(guess(mid) < 0) high = mid;
+            else low = mid;
         }
         
         return guess(low) == 0 ? low : high;
