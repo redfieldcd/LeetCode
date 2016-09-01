3b1e3fdbd6205904a8d9e0457d76c63581d84d3d
Dan Cao <withdancao@gmail.com>
2016-08-31 22:07:40 -0400

    Combination Sum IV

---
+++ 377-Combination-Sum-IV/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    int res = 0;
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for(int i = 0; i <= target; i++) {
+            for(int num : nums) {
+                if(i >= num) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        
+        return dp[target];
+    }
+}
