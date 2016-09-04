f92cb92203e3b0f8da84736bb25e5399074b9d63
Dan Cao <withdancao@gmail.com>
2016-09-04 16:10:30 -0400

    Median of Two Sorted Arrays

---
+++ 4-Median-of-Two-Sorted-Arrays/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums2 == null) return 0.0;
+        
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        
+        int mid = (len1 + len2) / 2;
+        
+        if((len1 + len2) % 2 == 1) {
+           return findk(nums1, 0, nums2, 0, mid + 1); 
+        } else {
+           return (findk(nums1, 0, nums2, 0, mid) + findk(nums1, 0, nums2, 0, mid+1)) / 2;
+        }
+    }
+    
+    public double findk(int[] nums1, int a, int[] nums2, int b, int k) {
+        if(a == nums1.length || (b < nums2.length && nums2[b] < nums1[a])) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+            int t = a;
+            a = b;
+            b = t;
+        }
+        
+        if(k == 1) return nums1[a];
+        
+        return findk(nums1, ++a, nums2, b, --k);
+    }
+}
