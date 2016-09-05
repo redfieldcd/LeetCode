50e01a7161037a7d6f99da57e789638ed77567af
Dan Cao <withdancao@gmail.com>
2016-09-05 15:22:24 -0400

    Generate Parentheses

---
+++ 22-Generate-Parentheses/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new LinkedList();
+        dfs(res, "", 0, 0, n);
+        return res;
+    }
+    
+    public void dfs(List<String> res, String path, int left, int right, int max) {
+        if(path.length() == 2 * max) {
+            res.add(path);
+            return;
+        }
+        
+        if(left < max) {
+            dfs(res, path + "(", left + 1, right, max);
+        }
+        
+        if(right < left) {
+            dfs(res, path + ")", left, right + 1, max);
+        }
+    }
+}
