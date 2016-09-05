d87e2fa9362608b078ba47f0b66d155c0a959d6c
Dan Cao <withdancao@gmail.com>
2016-09-05 18:43:41 -0400

    Binary Tree Inorder Traversal

--- 94-Binary-Tree-Inorder-Traversal/solution.java
+++ 94-Binary-Tree-Inorder-Traversal/solution.java
@@ -7,16 +7,47 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// recursion
+// public class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> list = new LinkedList();
+        
+//         if(root == null) return list;
+        
+//         list.addAll(inorderTraversal(root.left));
+//         list.add(root.val);
+//         list.addAll(inorderTraversal(root.right));
+        
+//         return list;
+//     }
+// }
+
+
+// Iteration
+// 先把左右的left都入栈
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new LinkedList();
+        Stack<TreeNode> s = new Stack();
         
-        if(root == null) return list;
-        
-        list.addAll(inorderTraversal(root.left));
-        list.add(root.val);
-        list.addAll(inorderTraversal(root.right));
-        
+        while(root != null || !s.isEmpty()) {
+            while(root != null) {
+                s.push(root);
+                root = root.left;
+            }
+            
+            root = s.pop();
+            list.add(root.val);
+            root = root.right;
+        }
         return list;
     }
 }
+
+
+
+
+
+
+
+
