dc9f984bdc0120c788a0776ed379e8e3284e6f4d
Dan Cao <withdancao@gmail.com>
2016-09-06 20:54:21 -0400

    Binary Tree Right Side View

--- 199-Binary-Tree-Right-Side-View/solution.java
+++ 199-Binary-Tree-Right-Side-View/solution.java
@@ -7,22 +7,59 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+ //Recursion
+// public class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> res = new LinkedList();
+        
+//         rightView(root, res, 0);
+        
+//         return res;
+//     }
+    
+//     public void rightView(TreeNode root, List<Integer> res, int level) {
+//         if(root == null) return;
+        
+//         if(res.size() == level) {
+//             res.add(root.val);
+//         }
+//         rightView(root.right, res, level + 1);
+//         rightView(root.left, res, level + 1);
+//     }
+// }
+
+//Iteration Based on level order traversal
 public class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList();
         List<Integer> res = new LinkedList();
         
-        rightView(root, res, 0);
+        q.offer(root);
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                
+                if(size - 1 == i) {
+                    res.add(node.val);
+                }
+                
+                if(node.left != null) {
+                    q.offer(node.left);
+                }
+                
+                if(node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+        }
         
         return res;
     }
-    
-    public void rightView(TreeNode root, List<Integer> res, int level) {
-        if(root == null) return;
-        
-        if(res.size() == level) {
-            res.add(root.val);
-        }
-        rightView(root.right, res, level + 1);
-        rightView(root.left, res, level + 1);
-    }
 }
+
+
+
