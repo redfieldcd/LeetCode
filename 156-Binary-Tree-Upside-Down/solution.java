0fdd11e257112f22dee2246132741be9c7d2c711
Dan Cao <withdancao@gmail.com>
2016-09-06 20:20:21 -0400

    Binary Tree Upside Down

--- 156-Binary-Tree-Upside-Down/solution.java
+++ 156-Binary-Tree-Upside-Down/solution.java
@@ -7,18 +7,41 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+ // Recursion
+// public class Solution {
+//     public TreeNode upsideDownBinaryTree(TreeNode root) {
+//         if(root == null || (root.left == null && root.right == null)) return root;
+        
+//         TreeNode newNode = upsideDownBinaryTree(root.left);
+        
+//         root.left.left = root.right;
+//         root.left.right = root;
+        
+//         root.left = null;
+//         root.right = null;
+        
+//         return newNode;
+//     }
+// }
+
+// Iteration
 public class Solution {
     public TreeNode upsideDownBinaryTree(TreeNode root) {
-        if(root == null || (root.left == null && root.right == null)) return root;
+        TreeNode curr = root;
+        TreeNode prev = null;
+        TreeNode next = null;
+        TreeNode temp = null;
         
-        TreeNode newNode = upsideDownBinaryTree(root.left);
+        while (curr != null) {
+            next = curr.left;
+            curr.left = temp;
+            temp = curr.right;
+            curr.right = prev;
+            prev = curr;
+            curr = next;
+        }
         
-        root.left.left = root.right;
-        root.left.right = root;
-        
-        root.left = null;
-        root.right = null;
-        
-        return newNode;
+        return prev;
     }
 }
