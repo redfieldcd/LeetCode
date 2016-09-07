1d47577ad260a725a5a40899acb6b4d89e587340
Dan Cao <withdancao@gmail.com>
2016-09-06 21:14:13 -0400

    [222. Count Complete Tree Nodes][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 222-Count-Complete-Tree-Nodes/solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int countNodes(TreeNode root) {
+        return countNodes(root, -1, -1);
+    }
+    
+    public int countNodes(TreeNode root, int left, int right) {
+        if(left == -1) {
+            left = 0;
+            TreeNode curr = root;
+            while(curr != null) {
+                curr = curr.left;
+                left++;
+            }
+        }
+        
+        if(right == -1) {
+            right = 0;
+            TreeNode curr = root;
+            while(curr != null) {
+                curr = curr.right;
+                right++;
+            }
+        }
+        
+        if(left == right) return (1 << left) - 1;
+        
+        //搜索左边就要把右子树搜索的高度设置为-1， 搜索右边就要把左子树搜索的高度设置为-1
+        return 1 + countNodes(root.left, left - 1, - 1) + countNodes(root.right, - 1, right - 1);  
+    }
+}
