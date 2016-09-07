111273e0dd81eb244c5071bc309d7ece8524238b
Dan Cao <withdancao@gmail.com>
2016-09-06 22:38:18 -0400

    Count Univalue Subtrees

--- 250-Count-Univalue-Subtrees/solution.java
+++ 250-Count-Univalue-Subtrees/solution.java
@@ -9,26 +9,26 @@
  */
 public class Solution {
     public int countUnivalSubtrees(TreeNode root) {
-        int count = 0;
+        int[] count = new int[1];
         isValid(root, count);
-        return count;
+        return count[0];
     }
     
-    public boolean isValid(TreeNode root, int count) {
+    public boolean isValid(TreeNode root, int[] count) {
         if(root == null) return true;
         
         boolean left = isValid(root.left, count);
         boolean right = isValid(root.right, count);
         
         if(left && right) {
-           if(root.left != null && root.left.val != root.val) {
+          if(root.left != null && root.left.val != root.val) {
                 return false;
             }
             
             if(root.right != null && root.right.val != root.val) {
                 return false;
             } 
-            count++;
+            count[0]++;
             return true;
         }
         return false;
