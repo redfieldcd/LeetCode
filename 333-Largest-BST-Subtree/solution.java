75b4f8f937017c858e4e0b64b78f31ada8828d42
Dan Cao <withdancao@gmail.com>
2016-09-07 20:47:05 -0400

    [333. Largest BST Subtree][Accepted]committed by LeetCode Extension

---
+++ 333-Largest-BST-Subtree/solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int largestBSTSubtree(TreeNode root) {
+        if(root == null) return 0;
+        if(isValid(root, Integer.MIN_VALUE, Integer.MAX_VALUE)) return countNode(root);
+        return Math.max(largestBSTSubtree(root.left), largestBSTSubtree(root.right));
+    }
+    
+    //判断以root为根的是不是BST
+    public boolean isValid(TreeNode root, Integer min, Integer max) {
+        if(root == null) return true;
+        if(min >= root.val) return false;
+        if(max <= root.val) return false;
+        
+        return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+    }
+    
+    public int countNode(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        return 1 + countNode(root.left) + countNode(root.right);
+    }
+}
