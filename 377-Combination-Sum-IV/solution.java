cf476e81c47fc0fedd9b3c28f571b344c67372db
Dan Cao <withdancao@gmail.com>
2016-09-09 22:47:59 -0400

    Combination Sum IV

--- 377-Combination-Sum-IV/solution.java
+++ 377-Combination-Sum-IV/solution.java
@@ -1,16 +1,33 @@
+// public class Solution {
+//     int res = 0;
+//     public int combinationSum4(int[] nums, int target) {
+//         int[] dp = new int[target + 1];
+//         dp[0] = 1;
+//         for(int i = 0; i <= target; i++) {
+//             for(int num : nums) {
+//                 if(i >= num) {
+//                     dp[i] += dp[i - num];
+//                 }
+//             }
+//         }
+        
+//         return dp[target];
+//     }
+// }
+
+
 public class Solution {
     int res = 0;
     public int combinationSum4(int[] nums, int target) {
-        int[] dp = new int[target + 1];
-        dp[0] = 1;
-        for(int i = 0; i <= target; i++) {
-            for(int num : nums) {
-                if(i >= num) {
-                    dp[i] += dp[i - num];
-                }
-            }
+        helper(nums, target);
+        return res;
+    }
+    
+    public void helper(int[] nums, int target) {
+        if(target < 0) return;
+        if(target == 0) res++;
+        for(int i = 0; i < nums.length; i++) {
+            helper(nums, target - nums[i]);
         }
-        
-        return dp[target];
     }
 }
