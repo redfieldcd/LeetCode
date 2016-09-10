d7a2277479cfbc67909ff1db53ab6d06979a1c1d
Dan Cao <withdancao@gmail.com>
2016-09-10 16:15:20 -0400

    [281. Zigzag Iterator][Runtime Error]committed by LeetCode Extension

--- 281-Zigzag-Iterator/solution.java
+++ 281-Zigzag-Iterator/solution.java
@@ -1,23 +1,49 @@
+// two vectors
+// public class ZigzagIterator {
+//     Iterator<Integer> iter1, iter2;
+//     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+//         iter1 = v1.iterator();
+//         iter2 = v2.iterator();
+//     }
+
+//     public int next() {
+//         if(iter1.hasNext()) {
+//             Iterator<Integer> temp = iter1;
+//             iter1 = iter2;
+//             iter2 = temp;
+//         }
+//         return iter2.next();
+//     }
+
+//     public boolean hasNext() {
+//         return iter1.hasNext() || iter2.hasNext();
+//     }
+// }
+
+//k vectors
 public class ZigzagIterator {
     Iterator<Integer> iter1, iter2;
+    Queue<Iterator> list;
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        iter1 = v1.iterator();
-        iter2 = v2.iterator();
+        list = new LinkedList();
+        list.add(v1.iterator());
+        list.add(v2.iterator());
     }
 
     public int next() {
-        if(iter1.hasNext()) {
-            Iterator<Integer> temp = iter1;
-            iter1 = iter2;
-            iter2 = temp;
+        Iterator<Integer> poll = list.poll();
+        int res = poll.next();
+        if(poll.hasNext()) {
+            list.add(poll);
         }
-        return iter2.next();
+        return res;
     }
 
     public boolean hasNext() {
-        return iter1.hasNext() || iter2.hasNext();
+       return !list.isEmpty();
     }
 }
+
 
 /**
  * Your ZigzagIterator object will be instantiated and called as such:
