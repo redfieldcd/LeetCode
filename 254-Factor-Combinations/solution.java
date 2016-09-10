e5622d75c0fe178c0c4d8255272675bb58cef179
Dan Cao <withdancao@gmail.com>
2016-09-09 23:10:26 -0400

    Factor Combinations

---
+++ 254-Factor-Combinations/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> res = new LinkedList();
+        List<Integer> path = new LinkedList();
+        
+        search(res, path, n, 2);
+        
+        return res;
+    }
+    
+    public void search(List<List<Integer>> res, List<Integer> path, int n, int index) {
+        if(n <= 1) {
+            if(path.size() > 1) {
+                List<Integer> temp = new LinkedList(path);
+                res.add(temp);
+            }
+            return;
+        }
+        
+        for(int i = index; i <= n; i++) {
+            if(n % i == 0) {
+                path.add(i);
+                search(res, path, n / i, i);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+}
