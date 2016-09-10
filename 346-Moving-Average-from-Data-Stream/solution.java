e7c43be35d7f7bddeaf6c5000c15d332791d8a49
Dan Cao <withdancao@gmail.com>
2016-09-10 14:54:34 -0400

    [346. Moving Average from Data Stream][Time Limit Exceeded]committed by LeetCode Extension

--- 346-Moving-Average-from-Data-Stream/solution.java
+++ 346-Moving-Average-from-Data-Stream/solution.java
@@ -1,35 +1,5 @@
-// public class MovingAverage {
-//     List<Integer> list;
-//     int n;
-//     /** Initialize your data structure here. */
-//     public MovingAverage(int size) {
-//         list = new LinkedList();
-//         n = size;
-//     }
-    
-//     public double next(int val) {
-//         list.add(val);
-//         if(list.size() < n) {
-//             int sum = 0;
-//             for(int i = 0; i < list.size(); i++) {
-//                 sum += list.get(i);
-//             }
-            
-//             return (double)sum / (double)list.size();
-//         } else {
-//             int sum = 0;
-//             for(int i = 0; i < n; i++) {
-//                 sum += list.get(list.size()-1- i);
-//             }
-            
-//             return (double)sum / (double)n;
-//         }
-//     }
-// }
-
 public class MovingAverage {
-    Double pre = 0.0;
-    Queue<Integer> list;
+    List<Integer> list;
     int n;
     /** Initialize your data structure here. */
     public MovingAverage(int size) {
@@ -38,15 +8,46 @@ public class MovingAverage {
     }
     
     public double next(int val) {
-        if(list.size() == n) {
-            pre -= list.remove();
-        }
-        
-        pre += val;
         list.add(val);
-        return (double)pre/list.size();
+        if(list.size() < n) {
+            int sum = 0;
+            for(int i = 0; i < list.size(); i++) {
+                sum += list.get(i);
+            }
+            
+            return (double)sum / (double)list.size();
+        } else {
+            int sum = 0;
+            for(int i = 0; i < n; i++) {
+                sum += list.get(list.size()-1- i);
+            }
+            
+            return (double)sum / (double)n;
+        }
     }
 }
+
+// 用一个Queue优化过后的方法
+// public class MovingAverage {
+//     Double pre = 0.0;
+//     Queue<Integer> list;
+//     int n;
+//     /** Initialize your data structure here. */
+//     public MovingAverage(int size) {
+//         list = new LinkedList();
+//         n = size;
+//     }
+    
+//     public double next(int val) {
+//         if(list.size() == n) {
+//             pre -= list.remove();
+//         }
+        
+//         pre += val;
+//         list.add(val);
+//         return (double)pre/list.size();
+//     }
+// }
 
 
 /**
