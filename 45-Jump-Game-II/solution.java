18d049394aca075ebbc1a08bda1a270124a0fa5b
Dan Cao <withdancao@gmail.com>
2016-09-12 19:47:44 -0400

    Jump Game II

---
+++ 45-Jump-Game-II/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int jump(int[] nums) {
+        int preHigh = 0, high = 0, low = 0, step = 0;
+        while(high < nums.length - 1) {
+            step++;
+            preHigh = high;
+            for(int i = low; i <= preHigh; i++) {
+                high = Math.max(high, i + nums[i]);
+            }
+            low = preHigh + 1;
+        }
+        
+        return step;
+    }
+}
