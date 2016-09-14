d837e6ecbf1238d02a69e1173a1d96ef10b40e14
Dan Cao <withdancao@gmail.com>
2016-09-13 21:20:19 -0400

    Missing Ranges

---
+++ 163-Missing-Ranges/solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> list = new LinkedList();
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(i == 0) {
+                if(nums[0] - lower == 1) {
+                    list.add("" + lower);
+                } else if(nums[0] - lower > 1) {
+                    int a = lower;
+                    int b = nums[0] - 1;
+                    list.add(""+ a + "->" + b);
+                }
+            } else {
+                if(nums[i] - nums[i-1] == 2) {
+                    int num = nums[i] - 1;
+                    list.add("" + num);
+                } else if ( nums[i] - nums[i-1] > 2) {
+                    int a = nums[i-1] + 1;
+                    int b = nums[i] - 1;
+                    list.add("" + a + "->" + b);
+                }
+            }
+        }
+        
+        if(upper - nums[nums.length - 1] == 1) {
+            list.add("" + upper);
+        } else if(upper - nums[nums.length - 1] > 1) {
+            int a = nums[nums.length - 1] + 1;
+            int b = upper;
+            list.add("" + a + "->" + b);
+        }
+        return list;
+    }
+}
