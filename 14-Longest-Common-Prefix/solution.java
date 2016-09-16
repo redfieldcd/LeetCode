c1ffc4101aa4904cc9c1b2c3715d8e7defbd4ab0
Dan Cao <withdancao@gmail.com>
2016-09-16 01:16:50 -0400

    [14. Longest Common Prefix][Accepted]committed by LeetCode Extension

--- 14-Longest-Common-Prefix/solution.java
+++ 14-Longest-Common-Prefix/solution.java
@@ -1,7 +1,6 @@
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
         if(strs == null || strs.length == 0) return "";
-        Arrays.sort(strs);
         String l = strs[0];
         for(int i = 1; i < strs.length; i++) {
             int j = 0;
