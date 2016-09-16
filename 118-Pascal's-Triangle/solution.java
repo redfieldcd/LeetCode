28e6cb06560b17088d906965b30aef01d6d55851
Dan Cao <withdancao@gmail.com>
2016-09-16 16:23:54 -0400

    Pascal's Triangle

---
+++ 118-Pascal's-Triangle/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> all = new ArrayList<List<Integer>>();
+        
+        for(int i = 0; i < numRows; i++){
+            List<Integer> row = new ArrayList<Integer>();
+            
+            for(int j = 0; j < i+1; j++){
+                if(j == 0 || j == i) row.add(1);
+                else row.add(all.get(i-1).get(j-1)+all.get(i-1).get(j));
+            }
+            all.add(row);
+        }
+        return all;
+     }
+}
