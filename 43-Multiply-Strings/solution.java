e4d31b3b8921860b6436878cdc8804a30af856cb
Dan Cao <withdancao@gmail.com>
2016-09-18 19:50:18 -0400

    Multiply Strings

--- 43-Multiply-Strings/solution.java
+++ 43-Multiply-Strings/solution.java
@@ -3,10 +3,10 @@ public class Solution {
         int n1 = num1.length(), n2 = num2.length();
         int[] p = new int[n1+n2];
         for(int i = n1 - 1; i >= 0; i--) {
-            for(int j = n1 - 1; j >= 0; j--) {
+            for(int j = n2 - 1; j >= 0; j--) {
                 int d1 = num1.charAt(i) - '0';
                 int d2 = num2.charAt(j) - '0';
-                p[i+j+1] = d1 * d2;
+                p[i+j+1] += d1 * d2;
             }
         }
         
