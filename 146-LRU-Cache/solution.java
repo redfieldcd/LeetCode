39fae6fcd39b26f8138088d6bcfa64b274251127
Dan Cao <withdancao@gmail.com>
2016-09-22 21:13:17 -0400

    LRU Cache

---
+++ 146-LRU-Cache/solution.java
@@ -0,0 +1,22 @@
+public class LRUCache {
+    private LinkedHashMap<Integer, Integer> map;
+    private final int Capacity;
+    
+    public LRUCache(int capacity) {
+        this.Capacity =  capacity;
+        this.map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > capacity;
+            }
+        }
+    }
+    
+    public int get(int key) {
+        if(map.contains(key)) return map.get(key);
+        else return -1;
+    }
+    
+    public void set(int key, int value) {
+        map.put(key, value);
+    }
+}
