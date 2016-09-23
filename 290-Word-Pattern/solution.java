4b03416e5d0477490e81d3e474e878efef579f93
Dan Cao <withdancao@gmail.com>
2016-09-23 00:48:37 -0400

    Word Pattern

--- 290-Word-Pattern/solution.java
+++ 290-Word-Pattern/solution.java
@@ -1,12 +1,27 @@
+// public boolean wordPattern(String pattern, String str) {
+//     String[] words = str.split(" ");
+//     if (words.length != pattern.length())
+//         return false;
+//     Map index = new HashMap();
+//     for (Integer i=0; i<words.length; ++i)
+//         if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+//             return false;
+//     return true;
+// }
+
 public class Solution {
     public boolean wordPattern(String pattern, String str) {
-        String[] words = str.split(" ");
-        if (words.length != pattern.length())
-            return false;
-        Map index = new HashMap();
-        for (Integer i=0; i<words.length; ++i)
-            if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+        String[] strs = str.split(" ");
+        
+        if(pattern.length() != strs.length) return false;
+        //map can be used for multiple type
+        Map map = new HashMap(); 
+        for(Integer i = 0; i < strs.length; ++i) {
+            // put returns value
+            if(map.put(pattern.charAt(i), i) != map.put(strs[i], i)) 
                 return false;
+        }
+        
         return true;
     }
 }
