151b8b4dbe114c187dcc18a146fec470bc77bd2a
Dan Cao <withdancao@gmail.com>
2016-09-23 00:56:08 -0400

    Word Pattern

--- 290-Word-Pattern/solution.java
+++ 290-Word-Pattern/solution.java
@@ -1,27 +1,35 @@
-// public boolean wordPattern(String pattern, String str) {
-//     String[] words = str.split(" ");
-//     if (words.length != pattern.length())
-//         return false;
-//     Map index = new HashMap();
-//     for (Integer i=0; i<words.length; ++i)
-//         if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
-//             return false;
-//     return true;
-// }
-
 public class Solution {
+    // public boolean wordPattern(String pattern, String str) {
+    //     String[] strs = str.split(" ");
+        
+    //     if(pattern.length() != strs.length) return false;
+    //     //map can be used for multiple type
+    //     Map map = new HashMap(); 
+    //     for(Integer i = 0; i < strs.length; ++i) {
+    //         // put object into map
+    //         if(map.put(pattern.charAt(i), i) != map.put(strs[i], i)) 
+    //             return false;
+    //     }
+        
+    //     return true;
+    // }
+    
     public boolean wordPattern(String pattern, String str) {
         String[] strs = str.split(" ");
         
         if(pattern.length() != strs.length) return false;
-        //map can be used for multiple type
-        Map map = new HashMap(); 
-        for(Integer i = 0; i < strs.length; ++i) {
-            // put returns value
-            if(map.put(pattern.charAt(i), i) != map.put(strs[i], i)) 
-                return false;
-        }
+        Map<Integer, String> map = new HashMap(); 
         
+        for(int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i)
+            if(map.containsKey(c)) {
+                if(!map.get(c).equals(strs[i])) return false;
+            } else {
+                if(map.containsValue(strs[i])) return false;
+                map.put(c, strs[i]);
+            }
+        }
+       
         return true;
     }
 }
