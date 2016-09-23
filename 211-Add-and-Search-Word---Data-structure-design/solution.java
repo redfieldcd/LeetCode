d3f53d498911081be050e7c0663352b57b62d697
Dan Cao <withdancao@gmail.com>
2016-09-22 20:46:28 -0400

    Add and Search Word - Data structure design

--- 211-Add-and-Search-Word---Data-structure-design/solution.java
+++ 211-Add-and-Search-Word---Data-structure-design/solution.java
@@ -14,7 +14,7 @@ public class WordDictionary {
         }
     }
     
-    TrieNode root = TrieNode();
+    TrieNode root = new TrieNode();
     // Adds a word into the data structure.
     public void addWord(String word) {
         TrieNode pointer = root;
