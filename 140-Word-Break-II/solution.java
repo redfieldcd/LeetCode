f8b05e7cd1ae5e2dcee307a09820e5b0ca90c786
Dan Cao <withdancao@gmail.com>
2016-09-23 15:52:27 -0400

    Word Break II

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -21,55 +21,55 @@
 //         }
 //     }
 // }
-// public class Solution {
-//     public List<String> wordBreak(String s, Set<String> dict) {
-//         Map<Integer, List<String>> validMap = new HashMap<Integer, List<String>>();
-
-//         // initialize the valid values
-//         List<String> l = new ArrayList<String>();
-//         l.add("");
-//         validMap.put(s.length(), l);
-
-//         // generate solutions from the end
-//         for(int i = s.length() - 1; i >= 0; i--) {
-//             List<String> values = new ArrayList<String>();
-//             for(int j = i + 1; j <= s.length(); j++) {
-//                 if (dict.contains(s.substring(i, j))) {
-//                     for(String word : validMap.get(j)) {
-//                         values.add(s.substring(i, j) + (word.isEmpty() ? "" : " ") + word);
-//                     }
-//                 }
-//             }
-//             validMap.put(i, values);
-//         }
-//         return validMap.get(0);
-//     }
-// }
-
 public class Solution {
-    public List<String> wordBreak(String s, Set<String> wordDict) {
-        Map<Integer, List<String>> map = new HashMap<Integer, List<String>>();
-        
-        List<String> l = new LinkedList();
+    public List<String> wordBreak(String s, Set<String> dict) {
+        Map<Integer, List<String>> validMap = new HashMap<Integer, List<String>>();
+
+        // initialize the valid values
+        List<String> l = new ArrayList<String>();
         l.add("");
-        map.put(s.length(), l);
-        
+        validMap.put(s.length(), l);
+
+        // generate solutions from the end
         for(int i = s.length() - 1; i >= 0; i--) {
-            List<String> values = new LinkedList();
+            List<String> values = new ArrayList<String>();
             for(int j = i + 1; j <= s.length(); j++) {
-                String subStr = s.substring(i, j);
-                if(wordDict.contains(subStr)) {
-                    for(String word : map.get(j)) {
-                        values.add(subStr + (word.isEmpty() ? "": " ") + word);
+                if (dict.contains(s.substring(i, j))) {
+                    for(String word : validMap.get(j)) {
+                        values.add(s.substring(i, j) + (word.isEmpty() ? "" : " ") + word);
                     }
                 }
             }
-            map.put(i, values);
+            validMap.put(i, values);
         }
-        
-        return map.get(0);
+        return validMap.get(0);
     }
 }
+
+// public class Solution {
+//     public List<String> wordBreak(String s, Set<String> wordDict) {
+//         Map<Integer, List<String>> map = new HashMap<Integer, List<String>>();
+        
+//         List<String> l = new LinkedList();
+//         l.add("");
+//         map.put(s.length(), l);
+        
+//         for(int i = s.length() - 1; i >= 0; i--) {
+//             List<String> values = new LinkedList();
+//             for(int j = i + 1; j <= s.length(); j++) {
+//                 String subStr = s.substring(i, j);
+//                 if(wordDict.contains(subStr)) {
+//                     for(String word : map.get(j)) {
+//                         values.add(subStr + (word.isEmpty() ? "": " ") + word);
+//                     }
+//                 }
+//             }
+//             map.put(i, values);
+//         }
+        
+//         return map.get(0);
+//     }
+// }
 
 
 
