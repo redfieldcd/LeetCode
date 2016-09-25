829c165e07e1c1213c65d093bf3bb3909eadc091
Dan Cao <withdancao@gmail.com>
2016-09-24 21:57:51 -0400

    Find the Difference

--- 389-Find-the-Difference/solution.java
+++ 389-Find-the-Difference/solution.java
@@ -1,15 +1,31 @@
+// public class Solution {
+//     public char findTheDifference(String s, String t) {
+//         int[] alpha = new int[26];
+        
+//         for (char c : s.toCharArray())
+//             alpha[ c - 'a' ]++;
+
+//         for (char c : t.toCharArray()) {
+//             if (--alpha[c - 'a'] < 0)
+//                 return c;
+//         }
+
+//         return 0;
+//     }
+// }
+
 public class Solution {
     public char findTheDifference(String s, String t) {
-        int[] alpha = new int[26];
+        char res = 0;
         
-        for (char c : s.toCharArray())
-            alpha[ c - 'a' ]++;
-
-        for (char c : t.toCharArray()) {
-            if (--alpha[c - 'a'] < 0)
-                return c;
+        for(char c : s.toCharArray()) {
+            res ^= c;
         }
-
-        return 0;
+        
+        for(char c : t.toCharArray()) {
+            res ^= c;
+        }
+        
+        return res;
     }
 }
