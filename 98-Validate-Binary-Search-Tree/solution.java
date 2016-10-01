86695ba041aefe29a2647deb3aec41747878d9b2
Dan Cao <withdancao@gmail.com>
2016-10-01 14:44:20 -0400

    [98. Validate Binary Search Tree][Runtime Error]committed by LeetCode Extension

--- 98-Validate-Binary-Search-Tree/solution.java
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -9,12 +9,13 @@
  */
 public class Solution {
     public boolean isValidBST(TreeNode root) {
-        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        if(root.left == null || root.right == null) return true;
+        return helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
     
     // check whether root.val is smaller than max and larger than min
     // check whetehr tree with root is a valid bst
-    public boolean helper(TreeNode root, long min, long max) {
+    public boolean helper(TreeNode root, int min, int max) {
         if(root == null) return true;
         
         if(root.val <= min || root.val >= max) {
