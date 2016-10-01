59e8f0449dc9c9f8b0f3376746c89689d0e33ecd
Dan Cao <withdancao@gmail.com>
2016-10-01 14:49:21 -0400

    [98. Validate Binary Search Tree][Compile Error]committed by LeetCode Extension

--- 98-Validate-Binary-Search-Tree/solution.java
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -7,20 +7,45 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// public class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+//     }
+    
+//     // check whether root.val is smaller than max and larger than min
+//     // check whetehr tree with root is a valid bst
+//     public boolean helper(TreeNode root, long min, long max) {
+//         if(root == null) return true;
+        
+//         if(root.val <= min || root.val >= max) {
+//             return false;
+//         }
+        
+//         return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+//     }
+// }
+
+// BFS
 public class Solution {
     public boolean isValidBST(TreeNode root) {
-        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-    
-    // check whether root.val is smaller than max and larger than min
-    // check whetehr tree with root is a valid bst
-    public boolean helper(TreeNode root, long min, long max) {
-        if(root == null) return true;
+        Queue<TreeNode> q = new Queue();
         
-        if(root.val <= min || root.val >= max) {
-            return false;
+        q.offer(root);
+        
+        while(!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if(node.left != null) {
+                if(node.left >= node.val) return false;
+                q.offer(node.left);
+            }
+            
+            if(node.right != null) {
+                if(node.right <= node.val) return false;
+                q.offer(node.right);
+            }
         }
         
-        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+        return true;
     }
+    
 }
