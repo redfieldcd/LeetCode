461b27395ccd28862f867ac7e2bdcfc62c379e1c
Dan Cao <withdancao@gmail.com>
2016-10-01 13:33:56 -0400

    [256. Paint House][Accepted]committed by LeetCode Extension

---
+++ 256-Paint-House/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int minCost(int[][] costs) {
+        int len = costs.length;
+        if(costs == null || len == 0) return 0;
+        
+        for(int i = 1; i < len; i++) {
+            costs[i][0] += Math.min(costs[i-1][1], costs[i-1][2]);
+            costs[i][1] += Math.min(costs[i-1][0], costs[i-1][2]);
+            costs[i][2] += Math.min(costs[i-1][0], costs[i-1][1]);
+        }
+        
+        return Math.min(Math.min(costs[len-1][0], costs[len-1][1]), costs[len-1][2]);
+    }
+}
