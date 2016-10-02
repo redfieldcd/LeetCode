a69788e9f32845d8559357648cc99239358bdc58
Dan Cao <withdancao@gmail.com>
2016-10-02 00:17:38 -0400

    [67. Add Binary][Accepted]committed by LeetCode Extension

--- 67-Add-Binary/solution.java
+++ 67-Add-Binary/solution.java
@@ -1,34 +1,8 @@
-// public class Solution {
-//     public String addBinary(String a, String b) {
-//         int i = a.length() - 1;
-//         int j = b.length() - 1;
-//         StringBuilder s = new StringBuilder();
-//         int carry = 0;
-        
-//         while(i >= 0 || j >= 0) {
-//             int m = i >= 0 ? a.charAt(i) - '0': 0;
-//             int n = j >= 0 ? b.charAt(j) - '0': 0;
-            
-//             int num = m + n + carry;
-            
-//             s.insert(0, String.valueOf(num % 2));
-            
-//             i--;
-//             j--;
-//             carry = num / 2;
-//         }
-        
-//         if(carry == 1) s.insert(0, String.valueOf(carry));
-        
-//         return s.toString();
-//     }
-// }
-
 public class Solution {
     public String addBinary(String a, String b) {
         int i = a.length() - 1;
         int j = b.length() - 1;
-        String res = "";
+        StringBuilder s = new StringBuilder();
         int carry = 0;
         
         while(i >= 0 || j >= 0) {
@@ -37,17 +11,43 @@ public class Solution {
             
             int num = m + n + carry;
             
-            res = String.valueOf(num % 2) + res;
+            s.insert(0, String.valueOf(num % 2));
             
             i--;
             j--;
             carry = num / 2;
         }
         
-        if(carry == 1) {
-           res = String.valueOf(carry) + res; 
-        }
+        if(carry == 1) s.insert(0, String.valueOf(carry));
         
-        return res;
+        return s.toString();
     }
 }
+
+// public class Solution {
+//     public String addBinary(String a, String b) {
+//         int i = a.length() - 1;
+//         int j = b.length() - 1;
+//         String res = "";
+//         int carry = 0;
+        
+//         while(i >= 0 || j >= 0) {
+//             int m = i >= 0 ? a.charAt(i) - '0': 0;
+//             int n = j >= 0 ? b.charAt(j) - '0': 0;
+            
+//             int num = m + n + carry;
+            
+//             res = String.valueOf(num % 2) + res;
+            
+//             i--;
+//             j--;
+//             carry = num / 2;
+//         }
+        
+//         if(carry == 1) {
+//           res = String.valueOf(carry) + res; 
+//         }
+        
+//         return res;
+//     }
+// }
