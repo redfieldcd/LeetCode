ee9ca3afda65ee87ccd8a6d52fbf6d7f7c8591c9
Dan Cao <withdancao@gmail.com>
2016-10-02 00:17:15 -0400

    [67. Add Binary][Accepted]committed by LeetCode Extension

--- 67-Add-Binary/solution.java
+++ 67-Add-Binary/solution.java
@@ -1,8 +1,34 @@
+// public class Solution {
+//     public String addBinary(String a, String b) {
+//         int i = a.length() - 1;
+//         int j = b.length() - 1;
+//         StringBuilder s = new StringBuilder();
+//         int carry = 0;
+        
+//         while(i >= 0 || j >= 0) {
+//             int m = i >= 0 ? a.charAt(i) - '0': 0;
+//             int n = j >= 0 ? b.charAt(j) - '0': 0;
+            
+//             int num = m + n + carry;
+            
+//             s.insert(0, String.valueOf(num % 2));
+            
+//             i--;
+//             j--;
+//             carry = num / 2;
+//         }
+        
+//         if(carry == 1) s.insert(0, String.valueOf(carry));
+        
+//         return s.toString();
+//     }
+// }
+
 public class Solution {
     public String addBinary(String a, String b) {
         int i = a.length() - 1;
         int j = b.length() - 1;
-        StringBuilder s = new StringBuilder();
+        String res = "";
         int carry = 0;
         
         while(i >= 0 || j >= 0) {
@@ -11,15 +37,17 @@ public class Solution {
             
             int num = m + n + carry;
             
-            s.insert(0, String.valueOf(num % 2));
+            res = String.valueOf(num % 2) + res;
             
             i--;
             j--;
             carry = num / 2;
         }
         
-        if(carry == 1) s.insert(0, String.valueOf(carry));
+        if(carry == 1) {
+           res = String.valueOf(carry) + res; 
+        }
         
-        return s.toString();
+        return res;
     }
 }
