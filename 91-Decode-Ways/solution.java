3dd0161d7c07044b2219011dbf828f71cd3f64a2
Dan Cao <withdancao@gmail.com>
2016-10-02 01:12:33 -0400

    [91. Decode Ways][Runtime Error]committed by LeetCode Extension

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,19 +1,23 @@
 public class Solution {
     public int numDecodings(String s) {
-        int len = s.length();
-        if (s == null || len == 0 || s.charAt(0) == '0') return 0;
-        
-        int[] dp = new int[len+1];
+        int[] dp = new int[s.length()+1];
         
         dp[0] = 1;
-        dp[1] = s.charAt(0) == 0 ? 0:1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
         
-        for(int i = 2; i <= len; i++) {
-            int num = Integer.parseInt(s.substring(i-2, i));
-            if(num >= 10 && num <= 26) dp[i] += dp[i-2];
-            if(s.charAt(i-1) != '0') dp[i] += dp[i-1];
+        for(int i = 2; i <= s.length(); i++) {
+            int first = Integer.parseInt(s.substring(i-1, i));
+            int second = Integer.parseInt(s.substring(i-2, i));
+            
+            if(first >= 1 && first <= 9) {
+                dp[i] += dp[i-1];
+            }
+            
+            if(second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
+            }
         }
         
-        return dp[len];
+        return dp[s.length()];
     }
 }
