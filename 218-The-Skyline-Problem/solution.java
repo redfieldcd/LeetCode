5e732b8d934082325d8bb9c4a2c489a1bf6fe4c8
Dan Cao <withdancao@gmail.com>
2016-10-03 00:41:44 -0400

    [218. The Skyline Problem][Accepted]committed by LeetCode Extension

--- 218-The-Skyline-Problem/solution.java
+++ 218-The-Skyline-Problem/solution.java
@@ -1,41 +1,43 @@
 public class Solution {
     public List<int[]> getSkyline(int[][] buildings) {
-        List<int[]> result = new LinkedList();
-        List<int[]> list = new LinkedList();
-        
-        for(int[] b : buildings) {
-            list.add(new int[]{b[0], -b[2]});
-            list.add(new int[]{b[1], b[2]});
+        List<int[]> result = new ArrayList<>();
+        List<int[]> height = new ArrayList<>();
+        // 拆解矩形，构建顶点的列表
+        for(int[] b:buildings) {
+            // 左顶点存为负数
+            height.add(new int[]{b[0], -b[2]});
+            // 右顶点存为正数
+            height.add(new int[]{b[1], b[2]});
         }
-        
-        Collections.sort(list, new Comparator<int[]>(){
-            public int compare(int[] l1, int[] l2) {
-                if(l1[0] != l2[0]) {
-                   return l1[0] - l2[0]; 
+        // 根据横坐标对列表排序，相同横坐标的点纵坐标小的排在前面
+        Collections.sort(height, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                if(a[0] != b[0]){
+                    return a[0] - b[0];
                 } else {
-                   return l1[1] - l2[0];
+                    return a[1] - b[1];
                 }
             }
         });
-        
-        Queue<Integer> q = new PriorityQueue(new Comparator<Integer>(){
-            public int compare(Integer i1, Integer i2) {
+        // 构建堆，按照纵坐标来判断大小
+        Queue<Integer> pq = new PriorityQueue<Integer>(11, new Comparator<Integer>(){
+            public int compare(Integer i1, Integer i2){
                 return i2 - i1;
             }
         });
-        
-        q.offer(0);
+        // 将地平线值9先加入堆中
+        pq.offer(0);
         // prev用于记录上次keypoint的高度
         int prev = 0;
-        for(int[] h:list) {
+        for(int[] h:height) {
             // 将左顶点加入堆中
             if(h[1] < 0) {
-                q.offer(-h[1]);
+                pq.offer(-h[1]);
             } else {
             // 将右顶点对应的左顶点移去
-                q.remove(h[1]);
+                pq.remove(h[1]);
             }
-            int cur = q.peek();
+            int cur = pq.peek();
             // 如果堆的新顶部和上个keypoint高度不一样，则加入一个新的keypoint
             if(prev != cur) {
                 result.add(new int[]{h[0], cur});
