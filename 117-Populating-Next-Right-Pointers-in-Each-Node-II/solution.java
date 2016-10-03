03500189d2b79f4e3d595d699c10a778e9791c70
Dan Cao <withdancao@gmail.com>
2016-10-03 17:18:28 -0400

    [117. Populating Next Right Pointers in Each Node II][Accepted]committed by LeetCode Extension

--- 117-Populating-Next-Right-Pointers-in-Each-Node-II/solution.java
+++ 117-Populating-Next-Right-Pointers-in-Each-Node-II/solution.java
@@ -6,40 +6,60 @@
  *     TreeLinkNode(int x) { val = x; }
  * }
  */
+ 
  public class Solution {
-    //based on level order traversal
     public void connect(TreeLinkNode root) {
-        TreeLinkNode head = null;
-        TreeLinkNode pre = null;
-        TreeLinkNode cur = root;
-        
-        while(cur != null) {
-            while(cur != null) {
-                if(cur.left != null) {
-                    if(pre != null) {
-                        pre.next = cur.left;
-                    } else {
-                        head = cur.left;
-                    }
-                    pre = cur.left;
-                }
-                
-                if(cur.right != null) {
-                    if(pre != null) {
-                        pre.next = cur.right;
-                    } else {
-                        head = cur.right;
-                    }
-                    pre =cur.right;
-                }
-                
-                cur = cur.next;
+        Queue<TreeLinkNode> q = new LinkedList<TreeLinkNode>();
+        if(root != null) q.offer(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                TreeLinkNode node = q.poll();
+                // 最后一个节点的next已经被初始为null，不用管它
+                if(i < size - 1) {
+                    TreeLinkNode next = q.peek();
+                    node.next = next;
+                }   
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
             }
-            
-            cur = head;
-            pre = null;
-            head = null;
         }
-        
     }
 }
+//  public class Solution {
+//     //based on level order traversal
+//     public void connect(TreeLinkNode root) {
+//         TreeLinkNode head = null;
+//         TreeLinkNode pre = null;
+//         TreeLinkNode cur = root;
+        
+//         while(cur != null) {
+//             while(cur != null) {
+//                 if(cur.left != null) {
+//                     if(pre != null) {
+//                         pre.next = cur.left;
+//                     } else {
+//                         head = cur.left;
+//                     }
+//                     pre = cur.left;
+//                 }
+                
+//                 if(cur.right != null) {
+//                     if(pre != null) {
+//                         pre.next = cur.right;
+//                     } else {
+//                         head = cur.right;
+//                     }
+//                     pre =cur.right;
+//                 }
+                
+//                 cur = cur.next;
+//             }
+            
+//             cur = head;
+//             pre = null;
+//             head = null;
+//         }
+        
+//     }
+// }
