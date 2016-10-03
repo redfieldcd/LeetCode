a092d734e28dbf01d4c5b2c091d6421fc801bc65
Dan Cao <withdancao@gmail.com>
2016-10-02 21:16:29 -0400

    [78. Subsets][Accepted]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
         List<List<Integer>> res = new LinkedList();
         List<Integer> temp = new LinkedList();
         
@@ -14,22 +14,11 @@ public class Solution {
             res.add(new LinkedList(temp));
             return;
         }
-        
+        //skip 
         helper(nums, idx+1, temp, res);
+        //add
         temp.add(nums[idx]);
         helper(nums, idx+1, temp, res);
         temp.remove(temp.size() - 1);
     }
-    
-    // public void helper(int[] nums, int idx, List<Integer> temp, List<List<Integer>> res) {
-    //     if(idx >= nums.length) return;
-    //     //不包含这个元素的
-    //     helper(nums, idx+1, temp, res);
-    //     //新的集合要new一个新的list，防止修改引用。
-    //     List<Integer> temp2 = new LinkedList(temp);
-    //     temp2.add(nums[idx]);
-    //     res.add(temp2);
-    //     //包含这个元素的
-    //     helper(nums, idx+1, temp2, res);
-    // }
 }
