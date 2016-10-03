df16305f5407ca8e4a73da369b6a8b5cfb3c9a9f
Dan Cao <withdancao@gmail.com>
2016-10-02 21:44:18 -0400

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -16,7 +16,11 @@ public class Solution {
     public boolean search(char[][] board, String word, int i, int j, int index) {
         if(index >= word.length()) return true;
         
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) return false;
+        //Check border
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length) return false;
+        
+        //Check value
+        if(board[i][j] != word.charAt(index)) return false;
         
         board[i][j] ^= 255;
         boolean res = search(board, word, i-1, j, index+1) || search(board, word, i, j-1, index+1) || search(board, word, i+1, j, index+1) || search(board, word, i, j+1, index+1);
