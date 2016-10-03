87114cef6b3ae7cd3705d0f115d1c2d75b4bffee
Dan Cao <withdancao@gmail.com>
2016-10-02 21:27:12 -0400

    [90. Subsets II][Accepted]committed by LeetCode Extension

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -1,29 +1,30 @@
 public class Solution {
+    Set<List<Integer>> set = new HashSet();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        // 对于没有重复的元素来说，sort无所谓
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList();
-        List<Integer> temp = new ArrayList();
-        res.add(temp);
-        // if(nums == null) return res;
+        List<List<Integer>> res = new LinkedList();
+        List<Integer> temp = new LinkedList();
         
-        helper(res, temp, 0, nums);
+        helper(nums, 0, temp, res);
         
         return res;
     }
     
-    public void helper(List<List<Integer>> res, List<Integer> temp, int index, int[] nums) {
-        if (index >= nums.length) return;
-        int old = index;
-        while(index < nums.length-1 && nums[index + 1] == nums[index]) index++;
-        helper(res, temp, index+1, nums);
-        for(int i = old; i <= index; i++) {
-            List<Integer> temp2 = new ArrayList<Integer>(temp);
-            for(int j = i; j <= index; j++) {
-               temp2.add(nums[index]); 
+    public void helper(int[] nums, int idx, List<Integer> temp, List<List<Integer>> res) {
+        if(idx == nums.length) {
+            if(!set.contains(temp)) {
+                res.add(new LinkedList(temp));
+                set.add(temp);
             }
-            res.add(temp2);
-            helper(res, temp2, index+1, nums);
+            
+            return;
         }
-       
+        //skip 
+        helper(nums, idx+1, temp, res);
+        //add
+        temp.add(nums[idx]);
+        helper(nums, idx+1, temp, res);
+        temp.remove(temp.size() - 1);
     }
 }
