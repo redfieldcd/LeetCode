9d3ae5261f13b6709db887b5dbf5e6ccc9959832
Dan Cao <withdancao@gmail.com>
2016-10-03 00:41:13 -0400

    [218. The Skyline Problem][Runtime Error]committed by LeetCode Extension

--- 218-The-Skyline-Problem/solution.java
+++ 218-The-Skyline-Problem/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
     public List<int[]> getSkyline(int[][] buildings) {
-        List<int[]> res = new LinkedList();
-        List<int[]> list = new LinedList();
+        List<int[]> result = new LinkedList();
+        List<int[]> list = new LinkedList();
         
         for(int[] b : buildings) {
-            list.add(new int{b[0], -b[2]});
-            list.add(new int{b[1], b[2]});
+            list.add(new int[]{b[0], -b[2]});
+            list.add(new int[]{b[1], b[2]});
         }
         
-        Collections.sort(list, new comparator(){
-            int compare(int[] l1, int[] l2) {
+        Collections.sort(list, new Comparator<int[]>(){
+            public int compare(int[] l1, int[] l2) {
                 if(l1[0] != l2[0]) {
                    return l1[0] - l2[0]; 
                 } else {
@@ -18,24 +18,24 @@ public class Solution {
             }
         });
         
-        Queue<Integer> q = new PriorityQueue(new comparator(){
-            int compare(Integer i1, Integer i2) {
+        Queue<Integer> q = new PriorityQueue(new Comparator<Integer>(){
+            public int compare(Integer i1, Integer i2) {
                 return i2 - i1;
             }
         });
         
-        pq.offer(0);
+        q.offer(0);
         // prev用于记录上次keypoint的高度
         int prev = 0;
         for(int[] h:list) {
             // 将左顶点加入堆中
             if(h[1] < 0) {
-                pq.offer(-h[1]);
+                q.offer(-h[1]);
             } else {
             // 将右顶点对应的左顶点移去
-                pq.remove(h[1]);
+                q.remove(h[1]);
             }
-            int cur = pq.peek();
+            int cur = q.peek();
             // 如果堆的新顶部和上个keypoint高度不一样，则加入一个新的keypoint
             if(prev != cur) {
                 result.add(new int[]{h[0], cur});
