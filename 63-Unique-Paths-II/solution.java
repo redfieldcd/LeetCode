e0bc6fd77f3fb499504b7836b1071139100ccff7
Dan Cao <withdancao@gmail.com>
2016-10-08 15:51:10 -0500

    Unique Paths II

---
+++ 63-Unique-Paths-II/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid == null || obstacleGrid.length == 0) return 0;
+        int a = obstacleGrid.length;
+        int b = obstacleGrid[0].length;
+        int[][] dp = new int[a][b];
+        
+        for(int i = 0; i < a; i++) {
+            if(obstacleGrid[i][0] = 0) {
+                dp[i][0] = 1;
+            } else {
+                break;
+            }
+        }
+        
+        for(int i = 0; i < b; i++) {
+            if(obstacleGrid[0][i] == 0) {
+                dp[0][i] = 1;
+            } else {
+                break;
+            }
+        }
+        
+        for(int i = 1; i < a; i++) {
+            for(int j = 1; j < b; j++) {
+                if(obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]; 
+                }
+            }
+        }
+        
+        return dp[a-1][b-1];
+    }
+}
