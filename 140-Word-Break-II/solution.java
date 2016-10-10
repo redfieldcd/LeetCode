9cfd935a26678964a8b10ac55c30afc0a3aaec59
Dan Cao <withdancao@gmail.com>
2016-10-10 14:16:32 -0400

    [140. Word Break II][Time Limit Exceeded]committed by LeetCode Extension

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -1,50 +1,25 @@
-// public class Solution {
-//     public List<String> wordBreak(String s, Set<String> wordDict) {
-//         List<String> res = new LinkedList();
-//         search(s, wordDict, 0, "", res);
-//         return res;
-//     }
-    
-//     public void search(String s, Set<String> wordDict, int index, String path, List<String> res) {
-//         if(index == s.length()) {
-//             res.add(path.trim());
-//             return;
-//         }
-        
-//         for(int i = index + 1; i <= s.length(); i++) {
-//             String subStr = s.substring(index, i);
-//             if(wordDict.contains(subStr)) {
-//                 path += subStr + " ";
-//                 search(s, wordDict, i, path, res);
-//                 path = path.substring(0, path.length() - subStr.length() - 1);
-//             } 
-//         }
-//     }
-// }
 public class Solution {
-    public List<String> wordBreak(String s, Set<String> dict) {
-        Map<Integer, List<String>> validMap = new HashMap<Integer, List<String>>();
-
-        // initialize the valid values
-        List<String> l = new ArrayList<String>();
-        l.add("");
-        validMap.put(s.length(), l);
-
-        // generate solutions from the end
-        for(int i = s.length() - 1; i >= 0; i--) {
-            List<String> values = new ArrayList<String>();
-            for(int j = i + 1; j <= s.length(); j++) {
-                if (dict.contains(s.substring(i, j))) {
-                    for(String word : validMap.get(j)) {
-                        values.add(s.substring(i, j) + (word.isEmpty() ? "" : " ") + word);
-                    }
-                }
-            }
-            validMap.put(i, values);
+    public List<String> wordBreak(String s, Set<String> wordDict) {
+        List<String> res = new LinkedList();
+        search(s, wordDict, 0, "", res);
+        return res;
+    }
+    
+    public void search(String s, Set<String> wordDict, int index, String path, List<String> res) {
+        if(index == s.length()) {
+            res.add(path.trim());
+            return;
         }
-        return validMap.get(0);
+        
+        for(int i = index + 1; i <= s.length(); i++) {
+            String subStr = s.substring(index, i);
+            if(wordDict.contains(subStr)) {
+                search(s, wordDict, i, path + subStr + " ", res);
+            } 
+        }
     }
 }
+
 
 // public class Solution {
 //     public List<String> wordBreak(String s, Set<String> wordDict) {
